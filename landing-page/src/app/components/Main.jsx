c628503cbda35bcb34866beab00445d4f3cfb555
Harsh Singh <harshsingh16d@gmail.com>
2024-07-08 21:38:35 +0530

    css changes

--- landing-page/src/app/components/Main.jsx
+++ landing-page/src/app/components/Main.jsx
@@ -4,7 +4,7 @@ export default function Main() {
 
     return (
         <div>
-            <div className='-mt-10 ml-32'>
+            <div className='-mt-10 ml-20'>
                 <Image src='/logoW.png' alt="LogoWhite" width={300} height={300} />
             </div>
 
@@ -17,14 +17,15 @@ export default function Main() {
                     <p className='text-gray-400'>
                         We are a team of enthusiastic individuals who help brands <br /> grow by unleashing the potential of social media
                     </p>
-                    <div className='flex gap-10'>
+                    <div className='flex gap-10 items-center'>
                         <button
-                            className="mt-10 relative px-8 py-2 rounded-md  isolation-auto z-10 border-2 border-red-700 before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full hover:text-white before:-right-full before:hover:right-0 before:rounded-full before:bg-[#A12347] before:-z-10 before:aspect-square before:hover:scale-150 overflow-hidden before:hover:duration-700 inline-flex items-center justify-center px-4 py-3 text-sm font-semibold text-black bg-white border border-gray-200 rounded-lg shadow-sm gap-x-2 hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none"
+                            className="mt-10 relative px-8 py-4 rounded-full  isolation-auto z-10 border-2 border-purple-700 before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full hover:text-white before:-right-full before:hover:right-0 before:rounded-full before:bg-purple-500 before:-z-10 before:aspect-square before:hover:scale-150 overflow-hidden before:hover:duration-700 inline-flex items-center justify-center text-sm font-semibold text-black bg-white shadow-sm gap-x-2 hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none"
                         >
                             Contact Us
                         </button>
-                        <button class="bg-red-950 text-red-400 border border-red-400 border-b-4 font-medium overflow-hidden relative px-4 py-2 rounded-md hover:brightness-150 hover:border-t-4 hover:border-b active:opacity-75 outline-none duration-300 group">
-                            <span class="bg-red-400 shadow-red-400 absolute -top-[150%] left-0 inline-flex w-80 h-[5px] rounded-md opacity-50 group-hover:top-[150%] duration-500 shadow-[0_0_10px_10px_rgba(0,0,0,0.3)]"></span>
+                        <button
+                            className="mt-10 relative px-8 py-4 rounded-full  isolation-auto z-10 border-2 border-red-700 before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full hover:text-white before:-right-full before:hover:right-0 before:rounded-full before:bg-[#A12347] before:-z-10 before:aspect-square before:hover:scale-150 overflow-hidden before:hover:duration-700 inline-flex items-center justify-center text-sm font-semibold text-black bg-white shadow-sm gap-x-2 hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none"
+                        >
                             View Brochure
                         </button>
                     </div>
