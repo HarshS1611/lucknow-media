cd51918b635c7903098e9f3db4dc0f0e5c769891
Harsh Singh <harshsingh16d@gmail.com>
2024-07-09 18:13:24 +0530

    fixes

--- landing-page/src/app/page.js
+++ landing-page/src/app/page.js
@@ -4,7 +4,7 @@ import Services from "./components/Services";
 
 export default async function Home() {
   await new Promise((resolve) => {
-    setTimeout(resolve, 3000);
+    setTimeout(resolve, 2000);
   });
   return (
     <div>
